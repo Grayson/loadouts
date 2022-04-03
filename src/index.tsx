2ecc6982b4c27ea5676c82748a8d54714dbd360b
Grayson <grayson.hansard@gmail.com>
2022-04-03 12:39:48 +0000

    Add endpoint to display post requests

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import express from 'express'
+import { renderToString } from 'react-dom/server'
+import DisplayPostRequest from './functions/display-post-request'
 
 const server = express()
+server.use(express.json())
+
 const port = 8080
 
 server.get('/info', (_, response) => {
 	response.json({
 		port
 	})
+})
+
+server.post('/display-post-request', (request, response) => {
+	const html = renderToString(DisplayPostRequest(request.body))
+	response.send(html)
 })
 
 server.listen(port, () => {
