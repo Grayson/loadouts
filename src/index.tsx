f684390b407dabc7d0e6f6c6accaad950ea09d90
Grayson <grayson.hansard@gmail.com>
2022-04-03 11:45:35 +0000

    Setup basic express server on port 8080

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,14 @@
-console.log("Hello, world!")
+import express from 'express'
+
+const server = express()
+const port = 8080
+
+server.get('/info', (_, response) => {
+	response.json({
+		port
+	})
+})
+
+server.listen(port, () => {
+	console.log(`Running on port ${port}`)
+})
